b0eb8dbc38713d73a619f79a2da31c382b432b78
Radcabna <radcabna@gmail.com>
2025-12-19 17:30:12 +0300

    Initial Commit

---
+++ Camping Checklist/Camping_ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camping_ChecklistApp.swift
+//  Camping Checklist
+//
+//  Created by Алкександр Степанов on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Camping_ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
